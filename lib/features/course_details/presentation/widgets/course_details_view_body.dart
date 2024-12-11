e76b848060a6c0d99d87b41ac0beb2b527117e68
aliNassef <anassef798@gmail.com>
2024-12-11 14:46:53 +0200

    create course details view

--- lib/features/course_details/presentation/widgets/course_details_view_body.dart
+++ lib/features/course_details/presentation/widgets/course_details_view_body.dart
@@ -1,10 +1,16 @@
 import 'package:e_learning_app/core/extensions/mediaquery_size.dart';
 import 'package:e_learning_app/core/shared/widgets/custom_network_image.dart';
+import 'package:e_learning_app/core/shared/widgets/default_app_button.dart';
 import 'package:e_learning_app/core/utils/app_constants.dart';
 import 'package:e_learning_app/core/utils/app_styles.dart';
 import 'package:e_learning_app/core/entity/course_entity.dart';
+import 'package:e_learning_app/features/course_details/presentation/widgets/custom_list_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:readmore/readmore.dart';
+
+import '../../../../core/shared/widgets/spacers.dart';
+import '../../../../core/utils/app_color.dart';
 
 class CourseDetailsViewBody extends StatelessWidget {
   const CourseDetailsViewBody({super.key, required this.course});
@@ -27,10 +33,86 @@ class CourseDetailsViewBody extends StatelessWidget {
               vertical: 8.h,
             ),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  course.courseDescription,
+                  course.category,
+                  style: AppStyles.textStyle14SB.copyWith(
+                    color: AppColors.secondaryColor,
+                  ),
+                ),
+                const VerticalSpace(8),
+                Text(
+                  'what will you learn in this course ?',
                   style: AppStyles.textStyle14SB,
+                ),
+                ReadMoreText(
+                  course.courseDescription,
+                  trimMode: TrimMode.Line,
+                  trimLines: 2,
+                  colorClickableText: AppColors.primaryColor,
+                  style: AppStyles.textStyle14R,
+                  trimCollapsedText: 'show more',
+                  trimExpandedText: 'show less',
+                  moreStyle: AppStyles.textStyle14R.copyWith(
+                    color: AppColors.primaryColor,
+                  ),
+                ),
+                const VerticalSpace(10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Discount : ',
+                      style: AppStyles.textStyle14SB,
+                    ),
+                    Text(
+                      '${course.discount} %',
+                      style: AppStyles.textStyle14SB.copyWith(
+                        color: AppColors.gray150Color,
+                      ),
+                    ),
+                  ],
+                ),
+                const VerticalSpace(16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Price : ',
+                      style: AppStyles.textStyle14SB,
+                    ),
+                    Text(
+                      '${int.parse(course.price) - (int.parse(course.price) * int.parse(course.discount) / 100)} EGP',
+                      style: AppStyles.textStyle14SB.copyWith(
+                        color: AppColors.gray150Color,
+                      ),
+                    ),
+                  ],
+                ),
+                const VerticalSpace(16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Insructor : ',
+                      style: AppStyles.textStyle14SB,
+                    ),
+                    Text(
+                      course.createdBy,
+                      style: AppStyles.textStyle14SB.copyWith(
+                        color: AppColors.gray150Color,
+                      ),
+                    ),
+                  ],
+                ),
+                const VerticalSpace(20),
+                const CustomListTile(),
+                const VerticalSpace(24),
+                const DefaultAppButton(
+                  text: 'Enroll Now',
+                  backgroundColor: AppColors.primaryColor,
+                  textColor: Colors.white,
                 ),
               ],
             ),
---
+++ lib/features/course_details/presentation/widgets/custom_list_tile.dart
@@ -0,0 +1,42 @@
+
+import 'package:e_learning_app/core/utils/app_styles.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../core/utils/app_color.dart';
+
+class CustomListTile extends StatelessWidget {
+  const CustomListTile({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: AppColors.primaryColor,
+        ),
+      ),
+      child: ListTile(
+        contentPadding: EdgeInsets.zero.copyWith(
+          left: 8.w,
+          right: 8.w,
+        ),
+        trailing: const Icon(
+          Icons.arrow_forward_ios,
+          color: AppColors.primaryColor,
+        ),
+        title: Text(
+          'Intro',
+          style: AppStyles.textStyle12SB,
+        ),
+        onTap: () {},
+        subtitle: Text(
+          'take a look at course content',
+          style: AppStyles.textStyle10R,
+        ),
+      ),
+    );
+  }
+}
